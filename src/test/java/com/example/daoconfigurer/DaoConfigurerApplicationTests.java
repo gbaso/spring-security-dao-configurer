7f27e2716ccc6df81430b1f66301702a6ee0a422
Giacomo Baso <giacomo.baso@sysdata.it>
2021-06-25 16:00:30 +0200

    add test case

--- src/test/java/com/example/daoconfigurer/DaoConfigurerApplicationTests.java
+++ src/test/java/com/example/daoconfigurer/DaoConfigurerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.daoconfigurer;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DaoConfigurerApplicationTests {
 
+	@Autowired
+    MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void daoAuthentication() throws Exception {
+		mockMvc.perform(get("/").with(httpBasic("user", "user"))).andExpect(status().isOk()).andExpect(content().string("Hello user!"));
 	}
 
 }
