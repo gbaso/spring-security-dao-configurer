36406e7282a6da2dea08d1df54bba538a03dcfc0
Giacomo Baso <giacomo.baso@sysdata.it>
2021-06-25 15:16:13 +0200

    initial commit

---
+++ src/main/java/com/example/daoconfigurer/DaoConfigurerApplication.java
@@ -0,0 +1,13 @@
+package com.example.daoconfigurer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoConfigurerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoConfigurerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daoconfigurer/DaoConfigurerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daoconfigurer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoConfigurerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
